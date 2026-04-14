c805d5e4c5f304a9fa726655957fb9e990cc411b
aryooww <setyoaryowiguno@gmail.com>
2026-04-14 15:20:51 +0700

    class MadiunMobil dan MadiunMotor

--- Aplikasi.java
+++ Aplikasi.java
@@ -19,3 +19,66 @@ abstract class LayananTransportasi {
     public abstract double hitungTarif();
 }
 
+class MadiunMotor extends LayananTransportasi {
+
+    public MadiunMotor(String namaDriver, double jarak, double saldoUser) {
+        super(namaDriver, jarak, saldoUser);
+    }
+
+    @Override
+    public double hitungTarif() {
+        return jarak * 2500;
+    }
+
+    public void prosesTransaksi() {
+        System.out.println("=== Transaksi oleh NAMA ANDA - NIM ANDA ===");
+
+        double total = hitungTarif();
+
+        if (saldoUser >= total) {
+            saldoUser -= total;
+
+            System.out.println("Driver: " + namaDriver);
+            System.out.println("Jarak: " + jarak + " km");
+            System.out.println("Total Bayar: Rp " + total);
+            System.out.println("Sisa Saldo: Rp " + saldoUser);
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+        }
+    }
+}
+
+class MadiunMobil extends LayananTransportasi {
+
+    private int nim;
+
+    public MadiunMobil(String namaDriver, double jarak, double saldoUser, int nim) {
+        super(namaDriver, jarak, saldoUser);
+        this.nim = nim;
+    }
+
+    @Override
+    public double hitungTarif() {
+        int duaDigit = nim % 100;
+        double biayaAdmin = 3000 + (duaDigit * 100);
+
+        return (jarak * 5000) + biayaAdmin;
+    }
+
+    public void prosesTransaksi() {
+        System.out.println("=== Transaksi oleh NAMA ANDA - NIM ANDA ===");
+
+        double total = hitungTarif();
+
+        if (saldoUser >= total) {
+            saldoUser -= total;
+
+            System.out.println("Driver: " + namaDriver);
+            System.out.println("Jarak: " + jarak + " km");
+            System.out.println("Total Bayar: Rp " + total);
+            System.out.println("Sisa Saldo: Rp " + saldoUser);
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+        }
+    }
+}
